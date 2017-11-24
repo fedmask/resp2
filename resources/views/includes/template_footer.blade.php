e761943cf08c3a98c242954ae6a994ea1ccf056e
giuseppebrb <giuseppe95barbato@gmail.com>
2017-11-24 20:30:28 +0100

    Aggiunto Footer e Informative
    Aggiunti sia il footer che dev'essere caricato quando viene effettuato il login che le varie pagine contenenti le informative. Inoltre sono stati caricati anche alcuni plugins presenti sulla vecchia piattaforma

---
+++ resources/views/includes/template_footer.blade.php
@@ -0,0 +1,10 @@
+<!--chiusura del  div aperto in template_header-->
+</div>
+
+     <!--END MAIN WRAPPER -->
+
+   <!-- FOOTER -->
+    <div id="footer">
+		<p><a href = "http://fsem.di.uniba.it/informative/consensoInformatoFsem.eu.html">Informativa privacy</a></p>
+        <p>Copyright © FSEM 2017</p>
+    </div>
