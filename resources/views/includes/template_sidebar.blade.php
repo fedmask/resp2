151ab71533ed5d2198ec0a4b7bfd475f72997b45
giuseppebrb <giuseppe95barbato@gmail.com>
2017-11-27 16:05:00 +0100

    Aggiunta barra di destra
    Tuttavia i valori inseriti nella barra di destra sono attualmente solo dei semplici placeholder

---
+++ resources/views/includes/template_sidebar.blade.php
@@ -0,0 +1,108 @@
+<!-- RIGHT STRIP  SECTION -->
+<div id="right">
+	<!--PARAMETRI DI MONITORAGGIO-->
+	<div class="panel panel-primary">
+		<div class="panel-body">
+			<ul class="list-small">
+				<li><strong>Altezza</strong>:
+					<span>
+						ALTEZZA-VAL
+					</span> m <span>(DATA INSERIMENTO ALTEZZA)</span>
+				</li>
+				<li><strong>Peso</strong>:
+					<span>
+						PESO-VAL
+					</span> kg <span>(DATA INSERIMENTO PESO)</span>
+				</li>
+				<li><strong>BMI</strong>:
+					<span>BMI-VAL</span>
+				<!-- TODO: implementare codice vecchio template -->
+					</span>
+				</li>
+				<li><strong>P.A.</strong>:
+					<span>PA-VAL</span>
+				<!-- TODO: implementare codice vecchio template -->
+					</span> <span>(DATA-PA>)</span>
+				</li>
+				<li><strong>F.C.</strong>:
+					<span>FC-VAL</span>
+				<!-- TODO: implementare codice vecchio template -->
+					</span> bpm <span>(DATA INSERIMENTO FC)</span>
+				</li>
+			</ul>
+		</div>
+	</div>
+	<!--FINE PARAMETRI DI MONITORAGGIO-->
+	<!--TERAPIE FARMACOLOGICHE IN CORSO-->
+	<div class="panel panel-primary">
+		<div class="panel-heading">Terapie farmacologiche</div>
+		<div class="panel-body">
+			<div class="table-responsive">
+				<small>
+					<table class="table">
+						<thead>
+							<tr>
+								<th>Farmaco</th>
+								<th>Data Principio</th>
+								<th>Data Fine</th>
+							</tr>
+						</thead>
+						<tbody>
+						<!-- TODO: implementare codice vecchio template -->
+						</tbody>
+					</table>
+				</small>
+			</div>
+		</div>
+	</div>
+	<!--FINE TERAPIE FARMACOLOGICHE IN CORSO-->
+	<!--FARMACI DA NON SOMMINISTRARE-->
+	<div class="panel panel-danger">
+		<div class="panel-heading">Farmaci da non somministrare</div>
+		<div class="panel-body">
+			<div class="table-responsive">
+				<small>
+					<table class="table">
+						<thead>
+							<tr>
+								<th>Farmaco</th>
+								<th>Motivo</th>
+							</tr>
+						</thead>
+						<tbody>
+							<tr>
+							<!-- TODO: implementare codice vecchio template -->
+							</tr>
+						</tbody>
+					</table>
+				</small>
+			</div>
+		</div>
+	</div>
+	<!--FINE FARMACI DA NON SOMMINISTRARE-->
+	<!--ANAMNESI PATOLOGICA-->
+	<div class="panel panel-primary">
+		<div class="panel-heading">Anamnesi Patologica (prossima e remota)</div>
+		<div class="panel-body">
+			<div class="table-responsive">
+				<small>
+					<table class="table">
+						<thead>
+							<tr>
+								<th>Patologia</th>
+								<th>Data Principio</th>
+								<th>Data Guarigione</th>
+								<th>stato</th>
+							</tr>
+						</thead>
+						<tbody>
+						<!-- TODO: implementare codice vecchio template -->
+						</tbody>
+					</table>
+				</small>
+			</div>
+		</div>
+	</div>
+	<!--FINE ANAMNESI PATOLOGICA-->
+</div>
+<!-- END RIGHT STRIP  SECTION -->
