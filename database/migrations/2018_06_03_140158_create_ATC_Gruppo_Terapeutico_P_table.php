f77b32452a773febefc006db83064afd09b5b352
Giuseppe Rossano <giuseppe.rossano95@gmail.com>
2018-06-03 16:10:48 +0200

    Aggiunte le Migration ATC_GAP, ATC_GTP

---
+++ database/migrations/2018_06_03_140158_create_ATC_Gruppo_Terapeutico_P_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateATCGruppoTerapeuticoPTable extends Migration
+{
+	
+	public $set_schema_table = 'ATC_Gruppo_Terapeutico_P';
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+    	Schema::create($this->set_schema_table, function (Blueprint $table) {
+            $table->char('Codice_Gruppo_Teraputico',2)->primary();
+            $table->timestamps();
+            $table->string('Descrizione',45);
+            $table->char('ID_Gruppo_Anatomico',1)->nullable(false);
+            
+            $table->foreign('ID_Gruppo_Anatomico', 'FOREIGN_GruppoT_GruppoA_idx')
+            ->references('ID_Gruppo_Anatomico')->on('tbl_Gruppo_Anatomico_P')
+            ->onDelete('no action')
+            ->onUpdate('no action');
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+    	Schema::dropIfExists($this->set_schema_table);
+    }
+}
