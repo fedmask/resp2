d4774803adfd974a4349559a57919d1f5e30a416
Giuseppe Rossano <giuseppe.rossano95@gmail.com>
2018-09-09 12:17:21 +0200

    Aggiunto il Middleware per il passaggio ad https

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -17,7 +17,9 @@ class Kernel extends HttpKernel {
 			\Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
 			\App\Http\Middleware\TrimStrings::class,
 			\Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
-			\App\Http\Middleware\TrustProxies::class 
+			\App\Http\Middleware\TrustProxies::class,
+			\App\Http\Middleware\HttpsProtocol::class
+			
 	];
 	
 	/**
@@ -36,7 +38,10 @@ class Kernel extends HttpKernel {
 					\Illuminate\View\Middleware\ShareErrorsFromSession::class,
 					\App\Http\Middleware\SessionErrors::class,
 					\App\Http\Middleware\VerifyCsrfToken::class,
-					\Illuminate\Routing\Middleware\SubstituteBindings::class 
+					\Illuminate\Routing\Middleware\SubstituteBindings::class,
+					\App\Http\Middleware\HttpsProtocol::class
+					
+			
 			],
 			
 			'api' => [ 
@@ -58,6 +63,7 @@ class Kernel extends HttpKernel {
 			'bindings' => \Illuminate\Routing\Middleware\SubstituteBindings::class,
 			'can' => \Illuminate\Auth\Middleware\Authorize::class,
 			'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
-			'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class 
+			'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
+			'HttpsProtocol' => \App\Http\Middleware\HttpsProtocol::class,
 	];
 }
