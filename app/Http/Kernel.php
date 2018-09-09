d3ce4dc188fdda3fed0a9474a44b9ebf23c735f6
Giuseppe Rossano <giuseppe.rossano95@gmail.com>
2018-09-09 18:07:04 +0200

    commentate le modifiche in Kernel.php

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -18,7 +18,9 @@ class Kernel extends HttpKernel {
 			\App\Http\Middleware\TrimStrings::class,
 			\Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
 			\App\Http\Middleware\TrustProxies::class,
-			\App\Http\Middleware\HttpsProtocol::class
+			
+			//Https middleware
+			//\App\Http\Middleware\HttpsProtocol::class
 			
 	];
 	
@@ -39,7 +41,9 @@ class Kernel extends HttpKernel {
 					\App\Http\Middleware\SessionErrors::class,
 					\App\Http\Middleware\VerifyCsrfToken::class,
 					\Illuminate\Routing\Middleware\SubstituteBindings::class,
-					\App\Http\Middleware\HttpsProtocol::class
+					
+					//Https middleware
+					//\App\Http\Middleware\HttpsProtocol::class
 					
 			
 			],
@@ -64,6 +68,8 @@ class Kernel extends HttpKernel {
 			'can' => \Illuminate\Auth\Middleware\Authorize::class,
 			'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
 			'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
-			'HttpsProtocol' => \App\Http\Middleware\HttpsProtocol::class,
+			
+			//Https middleware
+			//'HttpsProtocol' => \App\Http\Middleware\HttpsProtocol::class,
 	];
 }
