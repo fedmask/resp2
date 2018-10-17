22cb507a289ca60bc068ed46448e2097515a6fc6
Angelo Michele Lattanzio <michele.lattanzio.ml@gmail.com>
2018-10-17 13:37:08 +0200

    Inizio sviluppo view: "patientSearch.blade.php" dando la possibilità all'utente emergency di cercare i pazienti

---
+++ resources/views/pages/emergency/patientSearch.blade.php
@@ -0,0 +1,12 @@
+@extends( 'layouts.app' )
+@extends( 'includes.template_head' )
+
+@section( 'pageTitle', 'Pazienti' )
+@section( 'content' )
+
+<!--PAGE CONTENT -->
+   
+
+<!--END PAGE CONTENT --> -->
+
+@endsection
