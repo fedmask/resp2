ce382bdd6bd01db6f6eada31dd618d918e1ffff2
saver <saveriovalrosso@hotmail.com>
2019-02-24 18:04:22 +0100

    commit Valrosso

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -14,6 +14,23 @@ class ExampleTest extends TestCase
      */
     public function testBasicTest()
     {
+        $data = [
+            'name' => "New Product",
+            'description' => "This is a product",
+            'units' => 20,
+            'price' => 10,
+            'image' => "https://images.pexels.com/photos/1000084/pexels-photo-1000084.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+        ];
+        $user = factory(\App\User::class)->create();
+        $response = $this->actingAs($user, 'api')->json('POST', '/api/products',$data);
+
+
+        $response->assertStatus(200);
+        $response->assertJson(['status' => true]);
+        $response->assertJson(['message' => "Product Created!"]);
+        $response->assertJson(['data' => $data]);
+
+
         $this->assertTrue(true);
     }
 }
