38c5a9801fce2447edbfef2b9892bb29bd779176
Ivan Lamparelli <lamparelli.ivan@gmail.com>
2019-03-01 16:54:14 +0100

    Aggiunto test per il login relativo al controllo della presenza del messaggio di benvenuto (Ciao, nome_utente) in landing page in seguito al login

--- tests/Unit/LoginTest.php
+++ tests/Unit/LoginTest.php
@@ -5,11 +5,12 @@ namespace Tests\Unit;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use App\Http\Controllers\Auth\LoginController;
+use Auth;
 
 class LoginTest extends TestCase
 {
     var $validUserRedirect = '/home';
-    var $invalidUserRedirect = '/';
+    var $landingPage = '/';
 
     public function testValidPatientLogin() {
 
@@ -17,11 +18,18 @@ class LoginTest extends TestCase
         $validPatientPassword = 'test1234';
         $patientHomeView = 'pages.taccuino';
 
+        //Checks if the POST request redirects to /home
         $response = $this->login($validPatientId, $validPatientPassword);
         $response->assertRedirect($this->validUserRedirect);
 
+        //Checks if the redirect to /home is handled by HomeController and sends to the patient home
         $response = $this->call('GET', $this->validUserRedirect);
         $response->assertViewIs($patientHomeView);
+
+        //Checks the conditional content of the navbar in welcome.blade.php after the user is logged in
+        $welcomeMessage = 'Ciao, '.Auth::user()->utente_nome;
+        $response = $this->call('GET',$this->landingPage);
+        $response->assertSeeText($welcomeMessage);
     }
 
     public function testValidCareProviderLogin() {
@@ -31,14 +39,22 @@ class LoginTest extends TestCase
         $careProviderControllerRedirect = '/patients-list';
         $careProviderHomeView = 'pages.careprovider.patients';
 
+        //Checks if the POST request redirects to /home
         $response = $this->login($validCareProviderId, $validCareProviderPassword);
         $response->assertRedirect($this->validUserRedirect);
 
+        //Checks if the redirect to /home is handled by HomeController and sends to /patients-list
         $response = $this->call('GET', $this->validUserRedirect);
         $response->assertRedirect($careProviderControllerRedirect);
 
+        //Checks if the redirect /patients-list is handleded by CareProviderController and sends to the CP home
         $response = $this->call('GET', $careProviderControllerRedirect);
         $response->assertViewIs($careProviderHomeView);
+
+        //Checks the conditional content of the navbar in welcome.blade.php after the user is logged in
+        $welcomeMessage = 'Ciao, '.Auth::user()->utente_nome;
+        $response = $this->call('GET',$this->landingPage);
+        $response->assertSeeText($welcomeMessage);
     }
 
     public function testWrongPasswordLogin() {
@@ -46,8 +62,9 @@ class LoginTest extends TestCase
         $validPatientId = 'Janitor Jan';
         $invalidPatientPassword = 'wrongPassword';
 
+        //Checks if the POST request with invalid data redirects to /
         $response = $this->login($validPatientId, $invalidPatientPassword);
-        $response->assertRedirect($this->invalidUserRedirect);
+        $response->assertRedirect($this->landingPage);
     }
 
     public function testInvalidUserLogin() {
@@ -55,8 +72,9 @@ class LoginTest extends TestCase
         $invalidPatientId = 'wrongUser';
         $invalidPatientPassword = 'wrongPassword';
 
+        //Checks if the POST request with invalid data redirects to /
         $response = $this->login($invalidPatientId, $invalidPatientPassword);
-        $response->assertRedirect($this->invalidUserRedirect);
+        $response->assertRedirect($this->landingPage);
     }
 
     public function login($username, $password)
